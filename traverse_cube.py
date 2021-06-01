c8d00cbec0f2d11ca921ff1ee78f3a23beef27fc
hhakansson <henrik.hakansson@fcc.chalmer.se>
2021-06-01 22:11:48 +0200

    add args for traversing

--- traverse_cube.py
+++ traverse_cube.py
@@ -1,11 +1,18 @@
-from pipeline.common import filename
-from pipeline.traversing.traverser import EvaluationTraverser
+import argparse
+
 import numpy as np
 import torch
+
+from pipeline.traversing.traverser import EvaluationTraverser
 from definitions import config
 from pipeline.traversing.memory import max_batch_size
 
 from pipeline.segmentation.utils import get_base_segmenter, get_state_dict
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--i-job', type=int, default=0)
+parser.add_argument('--n-parallel', type=int, default=1)
+args = parser.parse_args()
 
 fits_file = config['traversing']['fits_file']
 
@@ -28,6 +35,6 @@ sofia_padding = np.array([8, 8, 50])
 mbatch = max_batch_size(segmenter.model, model_input_dim, config['traversing']['gpu_memory_max'])
 
 evaluator = EvaluationTraverser(segmenter, fits_file, model_input_dim, desired_dim, cnn_padding,
-                                sofia_padding, mbatch)
+                                sofia_padding, mbatch, n_parallel=args.n_parallel, i_job=args.i_job)
 
 df = evaluator.traverse()
